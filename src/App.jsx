67895d8803b5e54c9e8fd092e8e6348454882c90
VinoSubbu <vinothkumarsubburaman@gmail.com>
2024-07-20 11:30:48 +0530

    'link-initiated'

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
 import React from "react";
-import { BrowserRouter, Routes, Route , Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AxiosPage from "./components/AxiosPage";
 import Dashboard from "./components/Dashboard";
 // import CreateUser from "./components/CreateUser";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-  
   return (
-    <>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/axios" element={<AxiosPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          {/* <Route path="/create" element={<CreateUser />} /> */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />  
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/axios" element={<AxiosPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/create" element={<CreateUser />} /> */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../assets/styles/common.css'
+import { Link } from 'react-router-dom';
 
 function ColorSchemesExample() {
   return (
     <>
       <Navbar className='navbar-outer' >
-          <Navbar.Brand href="/" className='axois-logo'>Axois</Navbar.Brand>
+          <Link to="/" className='axois-logo'>Axois</Link>
           <Nav className='navbar-list'>
-            <Nav.Link href="/home" className='navbar-items'>Home</Nav.Link>
-            <Nav.Link href="/dashboard" className='navbar-items'>Dashboard</Nav.Link>
+            <Link to="/home" className='navbar-items'>Home</Link>
+            <Link to="/dashboard" className='navbar-items'>Dashboard</Link>
           </Nav>
       </Navbar>
     </>
